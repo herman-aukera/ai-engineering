cdb9cef86c8963912aeb10541b5be508729173ff
Herman Gundisalvus <57037259+herman-aukera@users.noreply.github.com>
2026-06-14 13:16:55 +0200

    feat: export source need contracts

--- estimador-cag/app/energy_chat/__init__.py
+++ estimador-cag/app/energy_chat/__init__.py
@@ -15,12 +15,15 @@ from app.energy_chat.contracts import (
     EnergyScore,
     EvaluationResult,
     RepairEvaluationResult,
+    SourceNeedRequest,
+    SourceNeedResult,
 )
 from app.energy_chat.evaluator import evaluate_answer, evaluate_with_one_pass_repair
 from app.energy_chat.reports import (
     build_deepseek_benchmark_report_markdown,
     write_deepseek_benchmark_report,
 )
+from app.energy_chat.source_guard import classify_source_need
 
 __all__ = [
     "CriticFinding",
@@ -37,7 +40,10 @@ __all__ = [
     "EnergyScore",
     "EvaluationResult",
     "RepairEvaluationResult",
+    "SourceNeedRequest",
+    "SourceNeedResult",
     "build_deepseek_benchmark_report_markdown",
+    "classify_source_need",
     "evaluate_answer",
     "evaluate_with_one_pass_repair",
     "write_deepseek_benchmark_report",
