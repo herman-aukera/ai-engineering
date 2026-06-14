2d610b86efb665c29d5a7c7b0c22c9da65657a79
Herman Gundisalvus <57037259+herman-aukera@users.noreply.github.com>
2026-06-14 19:38:30 +0200

    docs: include demo command checklist in export manifest

--- estimador-cag/scripts/export_energy_chat_manifest.sh
+++ estimador-cag/scripts/export_energy_chat_manifest.sh
@@ -22,6 +22,7 @@ Required paths:
   docs/energy_aware_chat_demo.md
   docs/energy_aware_chat_live_demo_readiness.md
   docs/energy_aware_chat_demo_script.md
+  docs/energy_aware_chat_demo_command_checklist.md
   docs/energy_aware_chat_api_smoke_guide.md
   docs/energy_aware_chat_demo_results_template.md
   docs/energy_aware_chat_reviewer_index.md
